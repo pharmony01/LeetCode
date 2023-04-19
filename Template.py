d240cd8da6ccc67fc4fb76774db1bad2871b5bbc
Riley Peters <rileypeters2016@gmail.com>
2023-04-19 18:14:18 -0400

    Added my template

---
+++ Template.py
@@ -0,0 +1,13 @@
+# URL
+# Author: PHarmony01
+def solve():
+    pass
+
+def main():
+    # Test cases
+    tcases = []
+    for t in tcases:
+        print(solve(t))
+
+if __name__ == "__main__":
+    main()
